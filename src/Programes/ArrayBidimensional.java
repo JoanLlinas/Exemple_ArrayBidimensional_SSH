01b911592aa8ff61e008b066b471b00a08c35ef3
Joan Llinàs Riera <jlr02@iesemilidarder.com>
2024-02-22 13:31:24 +0100

    primera versio

---
+++ src/Programes/ArrayBidimensional.java
@@ -0,0 +1,10 @@
+package Programes;
+
+public class ArrayBidimensional {
+
+	public static void main(String[] args) {
+		System.out.print("Esto es un array bidimensional, si no sabes no opines");
+
+	}
+
+}
